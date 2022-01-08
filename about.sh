10975b371bbcac99739e57e41443861b25ac38cd
vpnlegasi <97322084+vpnlegasi@users.noreply.github.com>
2022-01-08 14:25:32 +0800

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#       Premium Auto Script By VPN Legasi       #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# VPN Legasi PREMIUM                            #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "# ALLAH SWT                                     #"
+echo -e "# https://t.me/vpnlegasi                        #"
+echo -e "# My Family                                     #"
+echo -e "# Google                                        #"
+echo -e "# LauranceLE                                    #"
+echo -e "# v2ray My                                      #"
+echo -e "#                                               #"
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
