73f3a637ed05137571027fa9ebe516462f217896
vpnlegasi <97322084+vpnlegasi@users.noreply.github.com>
2022-01-08 14:42:03 +0800

    Add files via upload

--- renew-vless.sh
+++ renew-vless.sh
@@ -2,16 +2,8 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/vpnlegasi/ip/main/access | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-exit 0
-fi
+MYIP=$(wget -qO- icanhazip.com);
+
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/v2ray/vless.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
