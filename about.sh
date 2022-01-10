6d8427f9dd3bcedc6ea1cbf08c98a532859b482f
vpnlegasi <97322084+vpnlegasi@users.noreply.github.com>
2022-01-10 21:59:24 +0800

    Update about.sh

--- about.sh
+++ about.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/vpnlegasi/ip/main/access | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${red}Permission Denied!${NC}";
+echo "Only For Premium Users"
+exit 0
+fi
 clear
 echo -e "================================================="
 echo -e "#       Premium Auto Script By VPN Legasi       #"
