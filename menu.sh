77a674f7fd509fc83d446b5ee57cd18117b97e7f
vpnlegasi <97322084+vpnlegasi@users.noreply.github.com>
2022-01-11 14:54:29 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -166,7 +166,7 @@ echo -e  "$PURPLE  .-------------------------------------------------------."
 echo -e  "$PURPLE  |                 $yell Script By VPN Legasi$NC                 $PURPLE|"
 echo -e  "$PURPLE  '-------------------------------------------------------'$NC"
 echo -e   ""
-read -p "     Select From Options [1-62 or x] :  " opt
+read -p "     Select From Options [1-62 or x to exit] :  " opt
 echo -e   ""
 case $opt in
 1) clear ; usernew ;;
